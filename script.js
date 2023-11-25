1f8bd1d1699f04247f3a16dd169028cb5e4068e9
pranavikadatla <datlapranavika205@gmail.com>
2023-11-25 20:14:04 +0530

    my indexfile

---
+++ script.js
@@ -0,0 +1,73 @@
+     document.addEventListener('DOMContentLoaded',()=>{
+    const signupPage=document.getElementById("signup");
+    const profilePage=document.getElementById("profile");
+    const accessToken=localStorage.getItem('accessToken');
+   //   if(accessToken){
+   //       showProfilePage();
+   //   }else{
+   //   showSignupPage();
+   //  }
+const signupForm=document.getElementById("signupform")
+ signupForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+         const accessToken=generateRandomToken();
+        const password= document.getElementById("password").value;
+         const confirmpassword= document.getElementById("confirmpassword").value;
+        
+        if(password===confirmpassword){
+         console.log("hello");
+        const user={
+            userName:document.getElementById("username").value,
+            email:document.getElementById("email").value,
+            password:document.getElementById("password").value,
+            confirmpassword: document.getElementById("confirmpassword").value,
+        };
+        console.log(user);
+        localStorage.setItem('accessToken',accessToken);
+          localStorage.setItem('user',JSON.stringify(user));
+          showProfilePage();
+    }
+     else{
+        showErrorMessage("password and confirm password do not match.");
+    }
+
+     });
+   const errorMessage=document.getElementById("errormessage");
+      function showErrorMessage(message){
+        errorMessage.textContent=message;
+         errorMessage.style.color='red';
+
+     }
+
+     const logoutButton=document.getElementById("logoutbtn");
+     logoutButton.addEventListener("click",()=>{
+
+        localStorage.clear();
+        showSignupPage();
+     });
+     function showSignupPage(){
+        signupPage.style.display= 'block';
+        profilePage.style.display='none';
+     }
+     function showProfilePage(){
+        signupPage.style.display= 'none';
+        profilePage.style.display='block';
+        displayUserProfile();
+     }
+     function displayUserProfile(){
+        const user=JSON.parse(localStorage.getItem('user')); 
+        const userNameDisplay=document.getElementById("usernamedisplay") ;
+        const emailIdDisplay=document.getElementById("emailiddisplay");
+        const tokenDisplay=document.getElementById("tokendisplay");
+        const passwordDisplay=document.getElementById("passworddisplay") ;
+        userNameDisplay.textContent =`FullName : ${user.userName}`;
+        emailIdDisplay.textContent=`Email : ${user.email}`;
+        tokenDisplay.textContent=`Token :${accessToken}`;
+        passwordDisplay.textContent=`Password : ${user.password}`;
+     }
+
+     function generateRandomToken(){
+        return Array.from({length:16},()=>Math.floor(Math.random() * 256).toString(16).padStart(2, '0')).join('');
+     }
+    })  
+
